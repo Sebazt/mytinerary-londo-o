ef469c15155d3ed92659fece26998256f1ae4464
Sebazt <juan.londono29@udea.edu.co>
2022-02-16 16:29:38 -0500

    Inicializando proyecto con react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  let team = "Tortles"
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Team tortles
-        </a>
-      </header>
-    </div>
+    <>
+    
+    
+    
+    </>
   );
 }
 
